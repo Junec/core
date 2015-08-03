3dc27441b985956b91f6b04593a8aa1d7785fda1
June <594553417@qq.com>
2015-08-03 21:26:59 +0800

    bugfix.

--- request/factory.php
+++ request/factory.php
@@ -5,13 +5,13 @@
  * @author June Chen <594553417@qq.com>
  * @copyright  Copyright (c) 2015.
 */
-class core_request_client{
+class core_request_factory{
     private $instance;
 
     private function __construct(){}
     public  function __clone(){}
 
-    static public function factory($type = '',$timeout = 0){
+    static public function getInstance($type = '',$timeout = 0){
     	$client = "core_request_factory_{$type}";
         if(class_exists($client)){
         	return core::instance($client,$timeout);
